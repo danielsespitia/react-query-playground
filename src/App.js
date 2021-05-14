3062d1b02fa791b0dd69a08047eac8031d41bb6e
Daniel Espitia <daniel.espitiac@perficient.com>
2021-05-14 15:08:28 -0500

    added pokemon search and berries query

--- src/App.js
+++ src/App.js
@@ -1,42 +1,80 @@
+import React from 'react';
 import './App.css';
 import { useQuery } from 'react-query';
 import { ReactQueryDevtools } from 'react-query-devtools';
 
 import axios from 'axios';
-
+// just one fetch for two queries with the same data
 export default function App() {
+  const [pokemon, setPokemon] = React.useState('');
   return (
     <div>
-      <Pokemon queryKey='pokemon1'/>
-      <Pokemon queryKey='pokemon2'/>
+      <Count />
+      <input value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
+      <PokemonSearch pokemon={pokemon} />
+      <br />
+      <Pokemon />
+      <Berries />
       <ReactQueryDevtools />
     </div>
   );
 }
 
-function Pokemon({ queryKey }) {
+//Custom hook
+
+//React query useQueryhook to share data across entire app, easier than context because the context is handled behind the scenes throughout the app
+function usePokemon() {
   //1 arg -> Unique key for fetched data
   //2 arg -> Function that'ps gonna grab some data (normally fetch is used and receives an API as an argument)
   //when using fetch, be sure to get a json as a result (investigar promesas en detalle)
   //Loading... state
   //Errors
   //3 arg -> config object
+  return useQuery('pokemons', async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then((res) => res.data.results);
 
-  const queryInfo = useQuery(
-    queryKey,
-    async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      // if (true) {
-      //   throw new Error('Test error');
-      // }
-      return axios
-        .get('https://pokeapi.co/api/v2/pokemon')
-        .then((res) => res.data.results);
-    }
     //avoid refresh on browser window focus
     //stale time: avoid refreshing often
     //inactive: when it isn't showing in the app, cacheTime: time before removing inactive data from cache
+  });
+}
+
+function Count() {
+  const queryInfo = usePokemon();
+
+  return <h3>You are looking at {queryInfo.data?.length} pokemon</h3>;
+}
+
+function PokemonSearch({ pokemon }) {
+  const queryInfo = useQuery(pokemon, async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((res) => res.data);
+  });
+
+  return queryInfo.isLoading ? (
+    'Loading...'
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo.data?.sprites?.front_default ? (
+        <img src={queryInfo.data.sprites.front_default} alt="pokemon" />
+      ) : (
+        'Pokemon not found.'
+      )}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
   );
+}
+
+function Pokemon() {
+  const queryInfo = usePokemon();
 
   return queryInfo.isLoading ? (
     'Loading...'
@@ -47,6 +85,34 @@ function Pokemon({ queryKey }) {
       {queryInfo.data.map((result) => {
         return <div key={result.name}>{result.name}</div>;
       })}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  );
+}
+
+function Berries() {
+  const queryInfo = useQuery('berries', async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return axios
+      .get('https://pokeapi.co/api/v2/berry')
+      .then((res) => res.data.results);
+
+    //avoid refresh on browser window focus
+    //stale time: avoid refreshing often
+    //inactive: when it isn't showing in the app, cacheTime: time before removing inactive data from cache
+  });
+
+  return queryInfo.isLoading ? (
+    'Loading...'
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo.data.map((result) => {
+        return <div key={result.name}>{result.name}</div>;
+      })}
+      <br />
       {queryInfo.isFetching ? 'Updating...' : null}
     </div>
   );
