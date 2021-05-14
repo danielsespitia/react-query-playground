47d5eceec699b63fb9e76eaaddd5de8c1f2db120
Daniel Espitia <daniel.espitiac@perficient.com>
2021-05-14 10:47:47 -0500

    query keys and caching

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import { ReactQueryDevtools } from 'react-query-devtools';
 
 import axios from 'axios';
 
-function Pokemon() {
+export default function App() {
+  return (
+    <div>
+      <Pokemon queryKey='pokemon1'/>
+      <Pokemon queryKey='pokemon2'/>
+      <ReactQueryDevtools />
+    </div>
+  );
+}
+
+function Pokemon({ queryKey }) {
   //1 arg -> Unique key for fetched data
   //2 arg -> Function that'ps gonna grab some data (normally fetch is used and receives an API as an argument)
   //when using fetch, be sure to get a json as a result (investigar promesas en detalle)
@@ -13,7 +23,7 @@ function Pokemon() {
   //3 arg -> config object
 
   const queryInfo = useQuery(
-    'pokemon',
+    queryKey,
     async () => {
       await new Promise((resolve) => setTimeout(resolve, 1000));
       // if (true) {
@@ -22,10 +32,10 @@ function Pokemon() {
       return axios
         .get('https://pokeapi.co/api/v2/pokemon')
         .then((res) => res.data.results);
-    },
-    {
-      refetchOnWindowFocus: false,
     }
+    //avoid refresh on browser window focus
+    //stale time: avoid refreshing often
+    //inactive: when it isn't showing in the app, cacheTime: time before removing inactive data from cache
   );
 
   return queryInfo.isLoading ? (
@@ -37,15 +47,7 @@ function Pokemon() {
       {queryInfo.data.map((result) => {
         return <div key={result.name}>{result.name}</div>;
       })}
-    </div>
-  );
-}
-
-export default function App() {
-  return (
-    <div>
-      <Pokemon />
-      <ReactQueryDevtools/>
+      {queryInfo.isFetching ? 'Updating...' : null}
     </div>
   );
 }
