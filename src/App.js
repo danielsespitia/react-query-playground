82164b66a37b782a2cf79f376cd0f6e7f42ea435
Daniel Espitia <daniel.espitiac@perficient.com>
2021-05-14 17:58:22 -0500

    additional tests

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { useQuery } from 'react-query';
 import { ReactQueryDevtools } from 'react-query-devtools';
 
-import axios from 'axios';
+import axios, { CancelToken } from 'axios';
 // just one fetch for two queries with the same data
 export default function App() {
   const [pokemon, setPokemon] = React.useState('');
@@ -20,8 +20,55 @@ export default function App() {
   );
 }
 
-//Custom hook
+function PokemonSearch({ pokemon }) {
+  const queryInfo = useQuery(
+    ['pokemon', pokemon],
+    () => {
+      //repasar
+      const source = CancelToken.source();
 
+      const promise = new Promise((resolve) => setTimeout(resolve, 1000))
+        .then(() => {
+          return axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, {
+            cancelToken: source.token,
+          });
+        })
+        .then((res) => res.data);
+
+      promise.cancel = () => {
+        source.cancel('Query was cancelled by React Query');
+      };
+
+      return promise;
+    },
+    {
+      //set how many times  I want the request to retry
+      // retry: 1,
+      //set the time between retry attempts
+      // retryDelay: 1000,
+      // just enable the query when there's a pokemon
+      enabled: pokemon,
+    }
+  );
+
+  return queryInfo.isLoading ? (
+    'Loading...'
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo.data?.sprites?.front_default ? (
+        <img src={queryInfo.data.sprites.front_default} alt="pokemon" />
+      ) : (
+        'Pokemon not found.'
+      )}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  );
+}
+
+//Custom hook
 //React query useQueryhook to share data across entire app, easier than context because the context is handled behind the scenes throughout the app
 function usePokemon() {
   //1 arg -> Unique key for fetched data
@@ -46,31 +93,6 @@ function Count() {
   const queryInfo = usePokemon();
 
   return <h3>You are looking at {queryInfo.data?.length} pokemon</h3>;
-}
-
-function PokemonSearch({ pokemon }) {
-  const queryInfo = useQuery(pokemon, async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-      .then((res) => res.data);
-  });
-
-  return queryInfo.isLoading ? (
-    'Loading...'
-  ) : queryInfo.isError ? (
-    queryInfo.error.message
-  ) : (
-    <div>
-      {queryInfo.data?.sprites?.front_default ? (
-        <img src={queryInfo.data.sprites.front_default} alt="pokemon" />
-      ) : (
-        'Pokemon not found.'
-      )}
-      <br />
-      {queryInfo.isFetching ? 'Updating...' : null}
-    </div>
-  );
 }
 
 function Pokemon() {
